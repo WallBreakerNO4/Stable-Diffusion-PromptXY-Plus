e85eb1a6abe640331796a43fcfcc5706fe5aa0cf
WallBreakerNO4 <zhifanguo@gmail.com>
2024-11-13 03:53:21 +0800

    csv测试代码

---
+++ test_csv.py
@@ -0,0 +1,9 @@
+import csv
+from convert_braces import convert_braces
+
+with open('artist_strings.csv', 'r', encoding='utf-8-sig') as file:
+    reader = csv.reader(file)
+    artist_strings = []
+    for row in reader:
+        artist_strings.append(convert_braces(row[0]))
+    print(artist_strings)
