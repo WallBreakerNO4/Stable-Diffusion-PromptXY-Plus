f5bca2f4673a323bf5817152b236f56daed8e821
WallBreakerNO4 <zhifanguo@gmail.com>
2024-11-13 04:15:33 +0800

    同时使用artist_strings.csv和prompt_string.csv来构建json负载

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import csv
 from convert_braces import convert_braces
 from construct_json_payload import construct_json_payload_with_artist
 
-with open('artist_strings.csv', 'r', encoding='utf-8-sig') as file:
-    reader = csv.reader(file)
+with (open('artist_strings.csv', 'r', encoding='utf-8-sig') as artist_strings_csv,
+      open('prompt_string.csv', 'r', encoding='utf-8-sig') as prompt_string_csv):
+    artist_string_reader = csv.reader(artist_strings_csv)
+    prompt_string_reader = csv.reader(prompt_string_csv)
     artist_strings = []
-    for row in reader:
+    prompt_strings = []
+    for row in artist_string_reader:
         artist_strings.append(convert_braces(row[0]))
+    for row in prompt_string_reader:
+        prompt_strings.append(row[0])
     for artist_string in artist_strings:
-        print(construct_json_payload_with_artist(artist_string, ""))
+        for prompt_string in prompt_strings:
+            print(construct_json_payload_with_artist(artist_string, prompt_string))
+            # print(construct_json_payload_with_artist(artist_string, "||||just a test||||"))
