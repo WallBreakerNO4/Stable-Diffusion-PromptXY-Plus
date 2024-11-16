8c662db6cf384704dabc49bd5f3d797aeb57a847
WallBreakerNO4 <zhifanguo@gmail.com>
2024-11-17 02:03:02 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,16 +1,10 @@
-from lib.fetch_and_return_images import fetch_and_return_images
-from lib.construct_json_payload import *
+import pandas as pd
+import ast
 
-if __name__ == "__main__":
-    # 示例API调用地址和JSON内容，你需要根据实际情况进行调整
-    api_url = "http://192.168.20.100:7860/sdapi/v1/txt2img"
-    json_payload = construct_json_payload_with_artist(
-        r"{artist:kousaki ruri},[artist:wlop],artist:ciloranko,year 2023,",
-        r"1girl,texas \(arknights\),arknights,solider,tactic helmet,(military uniform,armored,ballistic,jacket,"
-        r"latex bodysuit underwear,skirt,black gloves),knee pads,elbow pads,boots,realistic pantyhose,")
-    print(json_payload)
-    # 调用函数
-    images = fetch_and_return_images(api_url, json_payload)
-    for idx, image in enumerate(images):
-        image.show()
-        print(f"Displaying image {idx + 1}")
+df = pd.read_csv('image_paths.csv', index_col=0)
+for prompt_string in df.index:
+    for artist_string in df.columns:
+        image_path = df.at[prompt_string, artist_string]
+        print("-----------------------")
+        print(f'Prompt: {prompt_string};\nArtist: {artist_string};\nImage Path: {image_path}')
+        print(type(image_path))
